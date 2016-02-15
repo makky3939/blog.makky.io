81e8c0bf5ff1be4c9739e3c96542b7468b98488a
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2016-02-15 15:44:04 +0900

    design footer

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,3 +8,9 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-47562530-2', 'blog.makky.io');
 ga('require', 'displayfeatures');
 ga('send', 'pageview');
+
+// Facebook SDK
+(function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;js = d.createElement(s); js.id = id;js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.5&appId=129245867430558";fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));
+
+// Twitter
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
